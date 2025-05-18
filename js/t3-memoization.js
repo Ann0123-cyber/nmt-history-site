aba22893626722ed4856b540cab7a50b4f35a4c2
Ann0123-cyber <anna.musiiovska@gmail.com>
2025-05-18 18:27:36 +0300

    completing 3 task

--- js/t3-memoization.js
+++ js/t3-memoization.js
@@ -1,20 +1,32 @@
 export class Cache {
   constructor(key) {
     this.key = key;
+    this.counterKey = `${key}-counter`;
   }
 
   get(field) {
-    const data = JSON.parse(localStorage.getItem(this.key)) || {};
+    let data = JSON.parse(localStorage.getItem(this.key)) || {};
     return data[field];
   }
 
   set(field, value) {
     const data = JSON.parse(localStorage.getItem(this.key)) || {};
     data[field] = value;
+
+    let saveCount = parseInt(localStorage.getItem(this.counterKey)) || 0;
+    saveCount += 1;
+
+    if (saveCount >= 10) {
+      this.clear();
+      saveCount = 0;
+    } else {
     localStorage.setItem(this.key, JSON.stringify(data));
+    }
+    localStorage.setItem(this.counterKey, saveCount.toString());
   }
 
   clear() {
     localStorage.removeItem(this.key);
+    localStorage.removeItem(this.counterKey);
   }
 }
